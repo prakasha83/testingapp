cfbd2343550ff46e21053cb4b89be926c1293bc9
prakash <prakash.ayyardevar@gmail.com>
2015-04-18 16:29:19 +0530

    updated project.

--- travels-entity/src/main/java/com/xcodefix/entity/model/type/EntityType.java
+++ travels-entity/src/main/java/com/xcodefix/entity/model/type/EntityType.java
@@ -2,15 +2,6 @@ package com.xcodefix.entity.model.type;
 
 public enum EntityType {
 	
-	CUSTOMER, EMPLOYEE;
-	private String desc;
-	
-//	private EntityType(String desc) {
-//		this.desc = desc;
-//	}
-	
-	public String getDesc() {
-		return desc;
-	}
+	CUSTOMER, EMPLOYEE
 
 }
